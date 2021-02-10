2e8ac9d7ddb5c405848e9a959e638b43e8260bc0
Mark Abdilla <markabdilla26@gmail.com>
2021-02-10 09:13:09 +0100

    Function added to decide winner of a round

--- game.js
+++ game.js
@@ -22,4 +22,46 @@ function computerPlay()
     return computerChoice;
 }
 
-console.log(computerPlay());
+function gameRound (playerChoice, computerChoice)
+{
+    if (playerChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "rock")
+    {
+        console.log("It's a tie!");
+    }
+    else if (playerChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "scissors")
+    {
+        console.log("It's a tie!");
+    }
+    else if (playerChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "paper")
+    {
+        console.log("It's a tie!");
+    }
+    else if (playerChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "paper")
+    {
+        console.log("You lose! Paper beats Rock!");
+    }
+    else if (playerChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "rock")
+    {
+        console.log("You lose! Rock beats Scissors!");
+    }
+    else if (playerChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "scissors")
+    {
+        console.log("You lose! Scissors beats Paper!");
+    }
+    else if (playerChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors")
+    {
+        console.log("You win!");
+    }
+    else if (playerChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper")
+    {
+        console.log("You win!");
+    }
+    else if (playerChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock")
+    {
+        console.log("You win!");
+    }
+    else
+    {
+        console.error("Your input was not recognised!")
+    }
+}
