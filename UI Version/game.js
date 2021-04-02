cdac2d9a4e33a3eb2db2f63a2417ea0fba67f3e8
Mark Abdilla <markabdilla26@gmail.com>
2021-04-02 16:31:51 +0200

    DOM cache and script preparation

---
+++ UI Version/game.js
@@ -0,0 +1,10 @@
+let userScore = 0;
+let computerScore = 0;
+
+const userScore_span = document.getElementById("user-score");
+const userScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock-btn");
+const paper_div = document.getElementById("paper-div");
+const scissors_div = document.getElementById("scissors-btn");
