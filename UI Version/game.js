79ed5432f6ea897d1f5267b20f00eca030ec276e
Mark Abdilla <markabdilla26@gmail.com>
2021-04-02 19:59:28 +0200

    Icons changed and final message appears when 5 points are reached

--- UI Version/game.js
+++ UI Version/game.js
@@ -1,35 +1,55 @@
 let userScore = 0;
 let computerScore = 0;
 
+let userWins = false;
+let computerWins = false;
+
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_p = document.querySelector(".result > p");
+const finalMessage = document.querySelector("#action-message");
 const rock_div = document.getElementById("rock-btn");
 const paper_div = document.getElementById("paper-btn");
 const scissors_div = document.getElementById("scissors-btn");
 
 function getComputerChoice()
 {
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     let randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
 function win(user, computer)
 {
-    userScore++;
-    userScore_span.innerHTML = userScore;
-    computerScore_span.innerHTML = computerScore;
-    result_p.textContent = user + " beats " + computer + " !";
+    if (userScore > 5)
+    {
+        userWins = true;
+        finalResult(user, computer);
+    }
+    else 
+    {
+        userScore++;
+        userScore_span.innerHTML = userScore;
+        computerScore_span.innerHTML = computerScore;
+        result_p.textContent = `${user} beats ${computer}!`;
+    }
 }
 
 function lose(userChoice, computerChoice)
 {
-    computerScore++;
-    userScore_span.innerHTML = userScore;
-    computerScore_span.innerHTML = computerScore;
-    result_p.textContent = computer + " beats " + user + " !";
+    if (computerScore > 5)
+    {
+        computerWins = true;
+        finalResult(user, computer);
+    }
+    else
+    {
+        computerScore++;
+        userScore_span.innerHTML = userScore;
+        computerScore_span.innerHTML = computerScore;
+        result_p.textContent = `${computer} beats ${user}!`;
+    }
 }
 
 function draw()
@@ -37,23 +57,35 @@ function draw()
     result_p.textContent = "We call that a draw!";
 }
 
+function finalResult(user, computer)
+{
+    if (userWins == true)
+    {
+        finalMessage.textContent = `Congratulations. ${user} saves the day! You win`;
+    }
+    else
+    {
+        finalMessage.textContent = `You lose. Your ${user} was no match for ${computer}.`;
+    }
+}
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice)
     {
-        case "rockscissors":
-        case "paperrock":
-        case "scissorspaper":
+        case "RockScissors":
+        case "PaperRock":
+        case "ScissorsPaper":
             win(userChoice, computerChoice);
             break;
-        case "rockpaper":
-        case "paperscissors":
-        case "scissorsrock":
+        case "RockPaper":
+        case "PaperScissors":
+        case "ScissorsRock":
             lose(userChoice, computerChoice);
             break;
-        case "rockrock":
-        case "paperpaper":
-        case "scissorsscissors":
+        case "RockRock":
+        case "PaperPaper":
+        case "ScissorsScissors":
             draw();
             break;
     }
@@ -62,15 +94,15 @@ function game(userChoice) {
 function main() 
 {
     rock_div.addEventListener('click', function(){
-        game("rock");
+        game("Rock");
     });
     
     paper_div.addEventListener('click', function(){
-        game("paper");
+        game("Paper");
     });
     
     scissors_div.addEventListener('click', function(){
-        game("scissors");
+        game("Scissors");
     });
 }
 
